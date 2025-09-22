68034fb1aa271f1b2144245c473b64f10d8efd3e
Aditya Palve <adityapalve518@gmail.com>
2025-09-22 15:32:33 +0530

    solved que

---
+++ Arrays/check_even_or_odd.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std ;
+int main(){
+    int n ;
+    cout<<"Enter the NUM : ";
+    cin>>n;
+    if(n%2==0){
+        cout<<n<<" is a even num "<<endl;
+    }
+    else{
+        cout<<n<<" Is a odd num "<<endl;
+    }
+    return 0 ;
+}
