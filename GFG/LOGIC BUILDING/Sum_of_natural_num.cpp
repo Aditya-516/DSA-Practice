df876f2c63d88a00c73a692658e79856d656ddbb
Aditya Palve <adityapalve518@gmail.com>
2025-09-22 16:04:48 +0530

    solved que

---
+++ GFG/LOGIC BUILDING/Sum_of_natural_num.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+using namespace std ;
+int main(){
+    int n ;
+    cout<<"Enter the NUM : ";
+    cin>>n;
+    int sum ;
+    sum = (n*(n+1))/2;
+    cout<<sum<<endl;
+    return 0 ;
+}
